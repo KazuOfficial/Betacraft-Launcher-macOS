a6599f672a06e3ab50acd16cad39c193199ac8e2
Jakub <jakub@kz-macos.local>
2020-04-25 16:12:54 +0200

    Files init

---
+++ Betacraft Launcher macoS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Betacraft Launcher macoS
+//
+//  Created by Jakub on 4/21/20.
+//  Copyright © 2020 Kazu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Betacraft Launcher macoS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Betacraft Launcher macoS
+//
+//  Created by Jakub on 4/21/20.
+//  Copyright © 2020 Kazu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
