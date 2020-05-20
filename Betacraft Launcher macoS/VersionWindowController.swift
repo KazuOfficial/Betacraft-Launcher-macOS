c5e972f1d0c8796ff812b0fc43ed16c3473052e3
Jakub <jakub@kz-macos.local>
2020-05-20 09:20:22 +0200

    Reading version list from the specified URL

--- Betacraft Launcher macoS/VersionWindowController.swift
+++ Betacraft Launcher macoS/VersionWindowController.swift
@@ -1,6 +1,6 @@
 //
 //  VersionWindowController.swift
-//  Betacraft Launcher macoS
+//  Betacraft Launcher macOS
 //
 //  Created by Jakub on 5/12/20.
 //  Copyright © 2020 Kazu. All rights reserved.
@@ -9,15 +9,34 @@
 import Cocoa
 
 class VersionWindowController: NSWindowController {
-
+    
+    @IBOutlet weak var myVersionsColumn: NSTableCellView!
+    @IBOutlet weak var myVersionsList: NSTableView!
+    var versions = ["shit"]
+    let url = URL(string: "http://betacraft.pl/launcher/assets/list.txt")
+    
     convenience init() {
         self.init(windowNibName: NSNib.Name(rawValue: "VersionWindowController"))
     }
     
     override func windowDidLoad() {
         super.windowDidLoad()
-
-        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        if let url = URL(string: "http://betacraft.pl/launcher/assets/list.txt") {
+            do {
+                let contents = try String(contentsOf: url)
+                versions.append(contents)
+                print(contents)
+            } catch {
+                //messagebox
+                print("Cannot load version list!")
+            }
+        }
+        
+        print(versions)
+    }
+    
+    func numberOfRows(myVersionsList: NSTableView) -> Int {
+        return versions.count
     }
     
 }
